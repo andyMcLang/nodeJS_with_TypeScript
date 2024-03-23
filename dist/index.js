c4cac47781d1fc2d1618d6cd77e5a6e81cee2499
Andy Lang <djdreviz@gmail.com>
2024-03-23 02:36:21 +0200

    Express Body-Parser to Parse Post Requests in Node with TypeScript done!

--- dist/index.js
+++ dist/index.js
@@ -5,8 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use(express_1.default.json());
 app.get('/', (req, res) => {
     res.send('Terve maailma!');
+});
+app.post('/', (req, res) => {
+    res.send({
+        data: req.body
+    });
 });
 app.listen(3000, () => {
     console.log('Kuunnellaan porttia 3000!');
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Terve maailma!');
+})
+
+app.post('/', (req, res) => {
+    res.send({
+        data: req.body
+    })
 })
 
 app.listen(3000, () => {

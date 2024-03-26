548d83e429df02b40eb5487947ef0aa0bc5732aa
Andy Lang <djdreviz@gmail.com>
2024-03-26 22:37:47 +0200

    Connect Remote MySQL Database in Node JS with TypeScript done

--- dist/index.js
+++ dist/index.js
@@ -4,23 +4,63 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mysql_1 = __importDefault(require("mysql"));
 const app = (0, express_1.default)();
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use(express_1.default.json());
-app.get('/Id/:id/Name/:name', (req, res) => {
-    res.send({
-        message: "Hellou maailma!",
-        data: req.params.id,
-        name: req.params.name
+app.get('/details/:id', (req, res) => {
+    var pool = mysql_1.default.createPool({
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'salasana',
+        database: 'andyl',
+        connectionLimit: 10,
+        multipleStatements: true,
     });
+    pool.getConnection(function (err, conn) {
+        if (err) {
+            console.log('Entered into error');
+            console.log(err);
+            res.send({
+                success: false,
+                statusCode: 500,
+                message: 'Getting error during the connection',
+            });
+            return;
+        }
+        console.log('The id: ' + req.params.id);
+        // if you got a connection...
+        conn.query('SELECT * FROM andyl.users WHERE user_id=?', [req.params.id], function (err, rows) {
+            if (err) {
+                conn.release();
+                return res.send({
+                    success: false,
+                    statusCode: 400,
+                });
+            }
+            // for simplicity, just send the rows
+            res.send({
+                message: 'Success',
+                statusCode: 200,
+                data: rows,
+            });
+            // CLOSE THE CONNECTION
+            conn.release();
+        });
+    });
+    //   res.send({
+    //     message: 'Hellou maailma!',
+    //     data: req.params.id,
+    //     name: req.params.name,
+    //   })
 });
 app.post('/Id/:id/Name/:name', (req, res) => {
     res.send({
         data: req.body,
         params: {
             id: req.params.id,
-            name: req.params.name
-        }
+            name: req.params.name,
+        },
     });
 });
 app.listen(3000, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,79 @@
-import express from 'express';
-import { Request, Response } from 'express';
+import express from 'express'
+import { Request, Response } from 'express'
+import mysql from 'mysql'
 
-const app = express();
-app.use(express.urlencoded({extended: false}))
+const app = express()
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.get('/Id/:id/Name/:name', (req: Request, res: Response) => {
-    res.send({
-        message: "Hellou maailma!",
-        data: req.params.id,
-        name: req.params.name
-    });
+app.get('/details/:id', (req: Request, res: Response) => {
+  var pool = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'salasana',
+    database: 'andyl',
+    connectionLimit: 10,
+    multipleStatements: true,
+  })
+
+  pool.getConnection(function (err: any, conn: any) {
+    if (err) {
+      console.log('Entered into error')
+      console.log(err)
+      res.send({
+        success: false,
+        statusCode: 500,
+        message: 'Getting error during the connection',
+      })
+
+      return
+    }
+
+    console.log('The id: ' + req.params.id)
+
+    // if you got a connection...
+    conn.query(
+      'SELECT * FROM andyl.users WHERE user_id=?',
+      [req.params.id],
+      function (err: any, rows: any) {
+        if (err) {
+          conn.release()
+          return res.send({
+            success: false,
+            statusCode: 400,
+          })
+        }
+
+        // for simplicity, just send the rows
+        res.send({
+          message: 'Success',
+          statusCode: 200,
+          data: rows,
+        })
+
+        // CLOSE THE CONNECTION
+        conn.release()
+      }
+    )
+  })
+
+  //   res.send({
+  //     message: 'Hellou maailma!',
+  //     data: req.params.id,
+  //     name: req.params.name,
+  //   })
 })
 
 app.post('/Id/:id/Name/:name', (req: Request, res: Response) => {
-    res.send({
-        data: req.body,
-        params: {
-            id: req.params.id,
-            name: req.params.name
-        }
-    })
+  res.send({
+    data: req.body,
+    params: {
+      id: req.params.id,
+      name: req.params.name,
+    },
+  })
 })
 
 app.listen(3000, () => {
-    console.log('Kuunnellaan porttia 3000!');
+  console.log('Kuunnellaan porttia 3000!')
 })
